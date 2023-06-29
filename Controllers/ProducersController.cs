5a6a720d1d5cd55a60b86c15835500445bf00801
johnpatrick-it <johnpatrickhaguimit@gmail.com>
2023-06-29 21:17:39 +0800

    Add project files.

---
+++ Controllers/ProducersController.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var allProducers = _context.Producers.ToList(); 
+            return View();
+        }
+    }
+}
